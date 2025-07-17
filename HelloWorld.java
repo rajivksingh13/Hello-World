07d5911f787758a26c957dfeb6b7e27b30802e17
Rajiv Kumar <rajeksingh2@gmail.com>
2025-07-17 21:41:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,9 @@ public class HelloWorld{
 	public static void main(String args[]) {
 
 		int MY_Var= 56;
+		int THIS_$_VAR = 900;
 
 		System.out.println("Hello I am feature ! print my-var ::"+My_VAR);
+		System.out.println("Hello I am feature ! print my-var ::"+THIS_$_VAR);
 	}
 }
